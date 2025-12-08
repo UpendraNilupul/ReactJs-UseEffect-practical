d094300285def2b67a3df596d943e52d27fa968f
Upendra Nilupul <139964152+UpendraNilupul@users.noreply.github.com>
2025-12-09 02:52:46 +0530

    Refactor App component to implement API call and remove unused assets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = ()=> {
+  const [apiId, setApiId] = useState('1');
+  const [data,setData] = useState({})
+  
+  console.log(apiId);
+  console.log(data);
+ 
+
+  useEffect(()=>{
+    console.log("Use effect running...");
+    
+    if (apiId.length > 0) {
+      console.log("useEffect if condition");
+
+      const apiCall = async() => {
+        const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${apiId}`)
+        const data = await res.json()
+
+        if(data) {
+          setData(data)
+        }
+
+      }
+
+      apiCall()
+      
+    }
+  },[apiId])
+
+  return(
+    <div>
+      <input type='text' value={apiId} onChange={(e)=> {setApiId(e.target.value)}} placeholder='enter id'></input>
     </div>
-  );
+  )
 }
 
 export default App;
