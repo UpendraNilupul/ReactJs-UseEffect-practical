bf72c587618c647a08a862a3bf596a60bc3ab63d
Upendra Nilupul <139964152+UpendraNilupul@users.noreply.github.com>
2025-12-09 23:18:22 +0530

    Refactor App component to improve API call logic and add search functionality

--- src/App.js
+++ src/App.js
@@ -4,19 +4,18 @@ import './App.css';
 
 const App = ()=> {
   const [apiId, setApiId] = useState('1');
+  const [search,setSearch] = useState(0);
   const [data,setData] = useState({})
   
-  console.log(apiId);
-  console.log(data);
+  //console.log(apiId);
+  //console.log(data);
  
 
   useEffect(()=>{
     console.log("Use effect running...");
-    
-    if (apiId.length > 0) {
-      console.log("useEffect if condition");
 
-      const apiCall = async() => {
+    const apiCall = async() => {
+        console.log("Api call running...")
         const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${apiId}`)
         const data = await res.json()
 
@@ -25,15 +24,28 @@ const App = ()=> {
         }
 
       }
-
-      apiCall()
-      
+    
+    if (apiId.length > 0 && Number(apiId)>0 && Number(apiId)<=100) {
+      console.log("useEffect if condition");
+      apiCall();
     }
+
+    return() => {
+      console.log("Cleanup..");
+      apiCall();
+    }
+
+
   },[apiId])
 
   return(
     <div>
       <input type='text' value={apiId} onChange={(e)=> {setApiId(e.target.value)}} placeholder='enter id'></input>
+      <button onClick={()=>setSearch(pre=>pre===0?1:0)}>Search</button>
+      {data&&(<div>
+        <h2>{data.title}</h2>
+        <p>{data.body}</p>
+      </div>)}
     </div>
   )
 }
